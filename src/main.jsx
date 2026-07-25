fccb9306a8a3a9a25f5c269f7c6e39bc71abda65
nitinsingh-pixel <ns5766609@gmail.com>
2026-07-25 12:33:45 -0700

    Fix case sensitivity in import paths

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppRoutes from './Routes/AppRoutes.jsx'
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./Context/AuthContext.jsx";
 
 createRoot(document.getElementById('root')).render(
  <AuthProvider> <AppRoutes/></AuthProvider>
